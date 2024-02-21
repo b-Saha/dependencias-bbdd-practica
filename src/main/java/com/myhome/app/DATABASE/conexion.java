54536412108acf0f262e7ed64dfbfa08a2dccbd5
Miriam Gutierrez Arribas <miriamga1994@gmail.com>
2024-02-21 19:06:59 +0100

    Sigo trabajando en el código, borro algunas clases que no son necesarias

--- src/main/java/com/myhome/app/DATABASE/conexion.java
+++ src/main/java/com/myhome/app/DATABASE/conexion.java
@@ -1,5 +1,9 @@
 package com.myhome.app.DATABASE;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Esta clase sirve para establecer conexión con la base de datos
  * @author Saha
@@ -7,5 +11,20 @@ package com.myhome.app.DATABASE;
  */
 
 public class conexion {
-        
+        //creamos una función que devuelve una conexión
+        public static Connection connect() throws SQLException{
+                try{
+                        //obtenemos los datos para conectarnos a la base de datos desde la clase configuracion
+                        var url = configuracion.getUrl();
+                        var user = configuracion.getUsername();
+                        var password = configuracion.getPassword();
+
+                        //abrimos y devolvemos la conexion con driverManager
+                        return DriverManager.getConnection(url, user, password);
+
+                }catch(SQLException exception) {
+                        System.err.println(exception.getMessage());
+                        return null;
+                }
+        }
 }
--- src/main/java/com/myhome/app/DATABASE/operaciones.java
+++
@@ -1,9 +0,0 @@
-package com.myhome.app.DATABASE;
-
-
-public class operaciones {
-    public int sumarNumeros(int num_x, int num_y) {
-        return num_x + num_y;
-    }
-}
-
